fedd1f5931c1ac83a2f6c06887b5cb2de98480f4
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-06-24 02:34:45 +0300

    all

--- markdown2html.py
+++ markdown2html.py
@@ -10,6 +10,34 @@ import os
 import sys
 
 
+def read_file(file):
+    """
+    reads the file
+    """
+    with open(file, 'r', encoding='utf-8') as f:
+        return f.readlines()
+
+
+def to_html(file_content):
+    """
+    Converts markdown to html
+    """
+    content = []
+    for line in file_content:
+        if line.startswith('#'):
+            parts = line.split(' ', 1)
+            level = len(parts[0])
+            title = parts[1].strip()
+            if 1 <= level <= 6:
+                content.append("<h{}>{}</h{}>"
+                               .format(level, title, level))
+            else:
+                content.append(line)
+        else:
+            content.append(line)
+    return '\n'.join(content)
+
+
 def main():
     """
     Takes in the arguments
@@ -27,9 +55,16 @@ def main():
     if not os.path.exists(markdown_file):
         print("Missing {}".format(markdown_file), file=sys.stderr)
         sys.exit(1)
-    
+
     # read the markdown file
-    
+    file_content = read_file(markdown_file)
+
+    # convert the markdown to html
+    html_content = to_html(file_content)
+
+    # write to output file
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write(html_content)
 
 
 if __name__ == "__main__":
